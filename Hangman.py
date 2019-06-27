2db48deef1d136e4a5295ceee2363683fd301a1a
IkBenDeAardappel <45482568+IkBenDeAardappel@users.noreply.github.com>
2019-06-27 16:40:23 +0200

    create hangman.py + woordenlijst

---
+++ Hangman.py
@@ -0,0 +1,3 @@
+WoordenLijst = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
+
+HetWoord = random.choice(WoordenLijst)
