89a21eb206f9c3fb1349bbd7dc99f258e479c169
IkBenDeAardappel <45482568+IkBenDeAardappel@users.noreply.github.com>
2019-06-27 17:02:58 +0200

    added LengteWoord + LengteAantal

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,11 @@
+import random
+
 WoordenLijst = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
 
 HetWoord = random.choice(WoordenLijst)
+LengteWoord = len(HetWoord)
+LetterAantal = "." * LengteWoord
+
+
+print("Het woord heeft " + str(LengteWoord) + " letters.")
+print(LetterAantal)
